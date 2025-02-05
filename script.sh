2f3c70e5b49d4f5414dd03c957fc0b8460ef9a6b
El Anderson <and10393@umn.edu>
2025-02-04 19:41:52 -0600

    Adds folders for outputs

--- script.sh
+++ script.sh
@@ -1,9 +1,8 @@
 # run from main directory using `./script.sh`
 
-for run_num in {1..30} # should be 30
+for run_num in {1..3} #should be 30
 do
     echo "This is run number $run_num"
-    cargo run --release knapsack_from_scratch > ./Outputs/ts_2_pID_Tiny/output_$run_num.txt
-    # make folder under Outputs folder for each round of tests, 
-    # then rename `ts_2_pID_Tiny` in above line with correct info
+    cargo run --release knapsack_from_scratch > ./Outputs/ts_2_pID_Big1/output_$run_num.txt
+    # update `ts_2_pID_Tiny` in above line with correct info
 done
